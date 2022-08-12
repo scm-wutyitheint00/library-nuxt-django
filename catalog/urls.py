8156a536fc36d9b02d13581c7656450e50113cc9
scm-wutyitheint <scm.dott.developer10@gmail.com>
2022-08-12 14:11:13 +0630

    add index page

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from django.urls import include
 
 urlpatterns = [
-
+    # path('catalog/', include('catalog.urls')),
+    path('', views.index, name='index'),
 ]
